9909e387a218754f1346b1fe52c9b6797944ab08
Probook <Probook@DESKTOP-BKFNJ69>
2024-02-03 09:16:31 +0700

    push program sederhana

---
+++ src/main/java/com/pbo/sederhana/Sederhana.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.sederhana;
+
+/**
+ *
+ * @author Probook
+ */
+public class Sederhana {
+
+    public static void main(String[] args) {
+        //deklarasi dan ititialisasi variable
+        String nim = "236512015";
+        String nama = "Muhammad Zaenal Abidin";
+        int angkatan = 2023;
+        double ipk = 4.00;
+        
+        //deklarasi dan initialisasi konstanta
+        final String PRODI = "Teknik Informatika";
+        
+        //Menampilkan isi variable
+        System.out.println("NIM :" +nim);
+        System.out.println("Nama  :" +nama);
+        System.out.println("Angkatan :" +angkatan);
+        System.out.println("IPK :" +ipk);
+        System.out.println("Prodi :" +PRODI);
+    }
+}
